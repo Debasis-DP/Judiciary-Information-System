ae86fa9d20165f27921a388071ceb3e8c7c86069
Vaishal <vaishalhshah3@gmail.com>
2016-03-30 18:08:38 +0530

    First Commit for Login

--- src/Login.java
+++ src/Login.java
@@ -1,10 +1,18 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
 
 public class Login {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -36,6 +44,48 @@ public class Login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, "name_48817473655117");
+		panel.setLayout(null);
+		
+		JButton btnCreateRegistrar = new JButton("Create Registrar");
+		btnCreateRegistrar.setBounds(61, 87, 199, 46);
+		panel.add(btnCreateRegistrar);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, "name_48894252615869");
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Username : ");
+		lblNewLabel.setBounds(33, 94, 90, 26);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblPassword = new JLabel("Password : ");
+		lblPassword.setBounds(33, 132, 102, 26);
+		panel_1.add(lblPassword);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(178, 39, 158, 24);
+		panel_1.add(comboBox);
+		
+		JLabel lblTypeOfUser = new JLabel("Type of User : ");
+		lblTypeOfUser.setBounds(33, 38, 102, 26);
+		panel_1.add(lblTypeOfUser);
+		
+		textField = new JTextField();
+		textField.setBounds(178, 94, 158, 26);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(178, 132, 158, 26);
+		panel_1.add(textField_1);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(33, 210, 117, 25);
+		panel_1.add(btnLogin);
 	}
-
 }
